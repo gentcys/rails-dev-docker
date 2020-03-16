de628240eeb7359d15b221805a9ced7326913c22
gentcys <iamchencong@gmail.com>
2020-03-16 13:56:26 +0800

    create user database for rails test

---
+++ mysql/docker-entrypoint-initdb.d/init.sql
@@ -0,0 +1,3 @@
+GRANT ALL PRIVILEGES ON activerecord_unittest.* to 'rails'@'localhost';
+GRANT ALL PRIVILEGES ON activerecord_unittest2.* to 'rails'@'localhost';
+GRANT ALL PRIVILEGES ON inexistent_activerecord_unittest.* to 'rails'@'localhost';
